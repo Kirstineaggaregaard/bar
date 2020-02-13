260b5f1a44808c4a23d6c389a899b6097047972b
Kirstine Aggergaard <kirstineaggergaard@hotmail.com>
2020-02-13 15:23:10 +0100

    now it works with 40 0's. still not working with array.length - error in line 28

--- bar.js
+++ bar.js
@@ -1,21 +1,31 @@
 "use strict";
 
-const myArray = [];
+window.addEventListener("DOMContentLoaded", count);
 
-function getRandom() {
-  return Math.floor(Math.random() * 32);
+// create array of 40 values
+let array = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+// const array = [];
+// array.length = 40;
+
+function count() {
+  console.log("count");
+  // get random numbers
+  let random = Math.floor(Math.random() * 32);
+  // add to the end of the array
+  array.push(random);
+  // remove first element in array
+  array.shift();
+  // set timer, to make movement and show bars
+  setTimeout(setHeight, 1000);
 }
 
 function setHeight() {
-  const random = getRandom();
+  console.log("setHeight");
   // find the first bar
-
-  for (let i = 0; i < myArray.length; i++) {
-    let bar;
-    const bar = document.querySelector(`#bars > div:nth-child(${i + 1})`);
+  for (let i = 0; i < array.length; i++) {
+    let bar = document.querySelector(`#bars > div:nth-child(${i + 1})`);
     // set the height of that bar with css style
-    bar.style.setProperty("--height", myArray[i]);
+    bar.style.setProperty("--height", array[i]);
   }
+  count();
 }
-
-setHeight();
